efb0ae732a4e6f639a504e5a5153172884f3840e
umutk <umut.keltek@gmail.com>
2020-01-08 17:45:57 +0300

    button_id has been changed with _id

--- main.py
+++ main.py
@@ -27,44 +27,54 @@ def show_all():
 @app.route("/my_api/all/<language>", methods=["GET"])
 def show_all_desired_language(language):
     output = []
-    all_elements = collection.find({}, {"_id": 0, "button_id": 1, language: 1})
+    all_elements = collection.find({}, {"_id": 1, language: 1})
     for data in all_elements:
         output.append(json.loads(json_util.dumps(data)))
     return jsonify(output)
 
 
-@app.route("/my_api/get/<button_id>", methods=["GET"])
-def get_button(button_id):
-    lng = collection.find_one({"button_id": button_id})
-    return dumps(lng)
+@app.route("/my_api/<_id>", methods=["GET"])
+def get_button(_id):
+    lng = collection.find_one({"_id": _id})
+    return jsonify(json.loads(json_util.dumps(lng)))
 
 
-@app.route("/my_api/delete/<button_id>", methods=["DELETE", "GET"])
-def delete_button(button_id):
-    lng = collection.find_one({"button_id": button_id})
+@app.route("/my_api/delete/<_id>", methods=["DELETE", "GET"])
+def delete_button(_id):
+    lng = collection.find_one({"_id": _id})
     collection.remove(lng)
     return "Button has been removed"
 
 
 @app.route("/my_api/add", methods=["POST"])
 def add_button():
+    field_name = request.json["_id"]
     user_json = request.json
-    collection.insert_one(user_json)
+    collection.insert_one(user_json, field_name)
     return "Added successfully"
 
 
-@app.route("/my_api/get/<button_id>/<language>", methods=["PUT", "GET"])
-def update_language_button(button_id, language):
+@app.route("/my_api/update/<_id>/<language>", methods=["PUT", "GET"])
+def update_language_button(_id, language):
     json_update = request.json[language]
-    x = collection.find_one_and_update({"button_id": button_id}, {"$set": {language: json_update}}, upsert=True,
+    x = collection.find_one_and_update({"_id": _id}, {"$set": {language: json_update}}, upsert=True,
                                        return_document=True)
     return "Your button has been updated"
 
 
-@app.route("/my_api/<language>/<button_id>", methods=["GET"])
-def get_specific_buttons_value(language, button_id):
-    x = collection.find({"button_id": button_id}, {"_id": 0, "button_id": 1, language: 1})
-    return dumps(x)
+@app.route("/my_api/get/<language>/<_id>", methods=["GET"])
+def get_specific_buttons_value(_id, language):
+    x = collection.find({"_id": _id}, {_id: 1, language: 1})
+    return jsonify(json.loads(json_util.dumps(x)))
+
+
+@app.route("/my_api/update/<_id>", methods=["PUT", "GET"])
+def update_all_language_field(_id):
+    user_json = request.json
+    x = collection.find_one_and_update({"_id": _id}, {"$set": user_json}, upsert=True,
+                                       return_document=True)
+
+    return "Your button has been updated"
 
 
 if __name__ == "__main__":
